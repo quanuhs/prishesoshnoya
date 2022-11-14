efd50abaaa0ece5954f06938b91b74aa76cc61e5
Gleb <37934662+quanuhs@users.noreply.github.com>
2022-11-14 21:45:38 +0000

    added keyboard

--- django_backend/telegramBot/logic/handle_request.py
+++ django_backend/telegramBot/logic/handle_request.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import types
-from . import texts, markups, text_buttons
+from . import texts, text_buttons
 
 from ..models import BotDictionary
 
@@ -31,75 +31,118 @@ def delete_inline_markup(user_id, call_message):
     return bot.delete_message(user_id, call_message)
 
 
-def generate_markup(buttons):
-    menu = types.InlineKeyboardMarkup()
-    menu.row_width = 1
-    for button in buttons:
-        menu.add(types.InlineKeyboardButton(button.title, callback_data={"button_id": button.id}))
-    return menu
-
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(get_user_id(message), texts.start_text,  reply_markup=markups.main_menu2, parse_mode='Markdown')
+    markup = Markups()
+    bot.send_message(get_user_id(message), texts.start_text,  reply_markup=markup.main_menu, parse_mode='Markdown')
     bot.send_message(get_user_id(message), texts.choice_start_text, parse_mode='Markdown')
 
 
-# @bot.callback_query_handler(func=lambda call: True)
-# def handle_callback(call):
-#     # Получаем id кнопки из call
-#     button_id = call.data.get("button_id")
-
-#     if button_id:
-#         BotButton.objects.
-        
-
 class Markups:
-    def __init__(self, bot_dictionary):
-        self.bot_dictionary = bot_dictionary
+    def __init__(self):
+        self._dict = BotDictionary.objects.first()
+    
+
+    @property
+    def main_menu(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
+
+        keyboard.add(
+            types.InlineKeyboardButton(self._dict.btn_process, callback_data='main_process'),
+            types.InlineKeyboardButton(self._dict.btn_bunt, callback_data='main_bunt'),
+            types.InlineKeyboardButton(self._dict.btn_prichess, callback_data='main_prich')
+            )
+
+        return keyboard
     
     @property
-    def main_process(self):
-        return []
+    def about_menu(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
 
+        keyboard.add(
+            types.InlineKeyboardButton(self._dict.btn_price, callback_data='price_ques'),
+            types.InlineKeyboardButton(self._dict.btn_contact, callback_data='write_us'),
+            types.InlineKeyboardButton(self._dict.btn_menu, callback_data='back_main_menu')
+        )
+
+        return keyboard
+    
+    @property
+    def subscribe_menu(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
+
+        keyboard.add(
+            types.InlineKeyboardButton(self._dict.btn_subscribe, callback_data='subscribe'),
+            types.InlineKeyboardButton(self._dict.btn_offerta, callback_data='offerta'),
+            types.InlineKeyboardButton(self._dict.btn_politics, callback_data='politics'),
+            types.InlineKeyboardButton(self._dict.btn_back, callback_data='back_about')
+        )
+
+        return keyboard
+    
+
+    @property
+    def pay_menu(self, generate_link):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
+
+        keyboard.add(
+            types.InlineKeyboardButton(self._dict.btn_pay, callback_data='pay', url=generate_link),
+            types.InlineKeyboardButton(self._dict.btn_back, callback_data='back_subs')
+        )
+
+        return keyboard
+
+
+    @property
+    def about_bunt(self):
+        keyboard = types.InlineKeyboardMarkup()
+        keyboard.row_width = 1
+
+        keyboard.add(
+            types.InlineKeyboardButton(self._dict.btn_bunt_channel, callback_data='channel_bunt', url=""),
+            types.InlineKeyboardButton(self._dict.btn_menu, callback_data='back_main_menu')
+        )
+
+        return keyboard
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def menu_callback(call):
-    bot_dictionary = BotDictionary.objects.first()
-
-    if bot_dictionary is None:
-        return
     
-    
+    markups = Markups()
     
     # Главное меню
     if call.data == "main_process":
-        bot.send_message(call.from_user.id, texts.about_text, reply_markup=markups.about_menu2, parse_mode='Markdown')
+        bot.send_message(call.from_user.id, texts.about_text, reply_markup=markups.about_menu, parse_mode='Markdown')
     
     elif call.data == "main_bunt":
-        bot.send_message(call.from_user.id, texts.about_bunt, reply_markup=markups.about_bunt2, parse_mode='Markdown')
+        bot.send_message(call.from_user.id, texts.about_bunt, reply_markup=markups.about_bunt, parse_mode='Markdown')
     
     elif call.data == "main_prich":
         pass
    
     elif call.data == "channel_bunt":
         pass
-    
-    
+
+
     # Меню about
     elif call.data == "price_ques":
-        bot.send_message(call.from_user.id, texts.price_text, reply_markup=markups.subs_menu2, parse_mode='Markdown')
+        bot.send_message(call.from_user.id, texts.price_text, reply_markup=markups.subscribe_menu, parse_mode='Markdown')
     
     elif call.data == "write_us":
         pass
     
     elif call.data == "back_main_menu":
-        bot.send_message(call.from_user.id, texts.start_text, reply_markup=markups.main_menu2, parse_mode='Markdown')
+        bot.send_message(call.from_user.id, texts.start_text, reply_markup=markups.main_menu, parse_mode='Markdown')
     
     
     # Меню подписки
     elif call.data == "subscribe":
-        bot.send_message(call.from_user.id, texts.subscribe_text, reply_markup=markups.pay_menu2, parse_mode="html")
+        bot.send_message(call.from_user.id, texts.subscribe_text, reply_markup=markups.pay_menu, parse_mode="html")
     
     elif call.data == "offerta":
         pass
@@ -108,7 +151,7 @@ def menu_callback(call):
         pass
     
     elif call.data == "back_about":
-        bot.send_message(call.from_user.id, texts.about_text, reply_markup=markups.about_menu2, parse_mode='Markdown')
+        bot.send_message(call.from_user.id, texts.about_text, reply_markup=markups.about_menu, parse_mode='Markdown')
 
     
     # Меню оплаты
@@ -116,10 +159,11 @@ def menu_callback(call):
         bot.send_message(call.from_user.id, texts.after_pay_text, parse_mode='html')
 
     elif call.data == "back_subs":
-        bot.send_message(call.from_user.id, texts.price_text, reply_markup=markups.subs_menu2, parse_mode='Markdown')
-
+        bot.send_message(call.from_user.id, texts.price_text, reply_markup=markups.subscribe_menu, parse_mode='Markdown')
 
     try:
+        
         bot.delete_message(call.message.chat.id, call.message.message_id)
+    
     except Exception as e:
         print(e)
